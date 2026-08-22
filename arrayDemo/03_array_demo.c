b762430973cea0f69cfd12549bbcc4e0edf827df
orange <2047320776@qq.com>
2026-08-22 21:20:55 +0800

    依旧数组练习

--- arrayDemo/03_array_demo.c
+++ arrayDemo/03_array_demo.c
@@ -3,12 +3,17 @@
 int main()
 {
     //获取变量的内存地址
-    int a=10;
-    printf("%p\n",&a);
-    int b=10;
-    printf("%p\n",&b);
-    int c=10;
-    printf("%p\n",&c);
+    // int a=10;
+    // printf("%p\n",&a);
+    // int b=10;
+    // printf("%p\n",&b);
+    // int c=10;
+    // printf("%p\n",&c);
+    int arr[]={1,2,3};
+    printf("%p\n",&arr);
+    printf("%p\n",&arr[0]);
+    printf("%p\n",&arr[1]);
+    printf("%p\n",&arr[2]);
 
 
     return 0;
---
+++ arrayDemo/04_array_demo.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void printfArr(int arr[],int len);
+
+int main()
+{
+    /*
+    数组作为函数参数要注意什么
+    实际上传递的是数组的首地址，如果要在函数中对数组进行遍历的话，一定要把数组的长度传递过去
+    定义处：arr表示的是完整的数组
+    函数中的arr：只是一个变量，用来记录数组的首地址
+    */
+    //定义数组
+    int arr[]={1,2,3,4,5};
+    //printf("%zu\n",sizeof(arry));
+    int len=sizeof(arr)/sizeof(arr[0]);
+    //调用函数遍历数组
+    printfArr(arr,len);
+
+    return 0;
+}
+
+void printfArr(int arr[],int len)
+{
+    
+    for (int i=0;i<len;i++)
+    {
+        printf("%d\n",arr[i]);
+    }
+}
